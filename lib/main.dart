5d7c2b35a115054d96d966f9ca189b2e91a95027
lempurdysh <ryuote@gmail.com>
2025-05-22 22:42:41 +0300

    added color changing functionality, styled the text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -32,23 +33,69 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   Color? backgroundColor;
 
+  Color? computeTextColor() {
+    if (backgroundColor == null) return null;
+
+    if (backgroundColor!.computeLuminance() < 0.25) {
+      return Colors.white;
+    }
+
+    return Colors.black;
+  }
+
+  void randomiseBackgroundColor() {
+    setState(() {
+      backgroundColor = Color(
+        (Random().nextDouble() * 0xFFFFFF).toInt(),
+      ).withOpacity(1.0);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        setState(() {
-          backgroundColor = Color(
-            (Random().nextDouble() * 0xFFFFFF).toInt(),
-          ).withOpacity(1.0);
-        });
-      },
+      onTap: randomiseBackgroundColor,
       child: Scaffold(
         backgroundColor:
             backgroundColor ?? Theme.of(context).colorScheme.surface,
         body: Center(
-          child: Text(
-            'Hello There',
-            style: Theme.of(context).textTheme.titleLarge,
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 32,
+              vertical: 16,
+            ),
+            decoration: BoxDecoration(
+              backgroundBlendMode: BlendMode.lighten,
+              gradient: const LinearGradient(
+                colors: [
+                  Colors.white10,
+                  Colors.white10,
+                  Colors.white12,
+                  Colors.white24,
+                ],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+              ),
+              boxShadow: const [
+                BoxShadow(
+                  blurRadius: 8,
+                  spreadRadius: 2,
+                  color: Colors.black12,
+                  offset: Offset(4, 8),
+                )
+              ],
+              border: Border.all(
+                color: Colors.white24,
+                width: 1,
+              ),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Text(
+              'Hello There',
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                    color: computeTextColor(),
+                  ),
+            ),
           ),
         ),
       ),
